c9ebf7428e1cd93622608c9b4003059f178fa5fd
Gustavo Larsen <gustavo.larsen@cwi.com.br>
2024-10-16 15:16:58 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview'; // Importando o tipo
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(
+    null
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const requestCameraPermission = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+      setIsLoading(false); // Atualiza o estado de carregamento após solicitar permissão
+    };
+
+    requestCameraPermission();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color="#0000ff"
+      />
+    );
+  }
+
+  if (hasCameraPermission === false) {
+    return <Text>Permissão de câmera negada.</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        source={{
+          uri: 'https://gentle-wave-0f628b70f-preview.eastus2.5.azurestaticapps.net/'
+        }}
+        style={{ flex: 1 }}
+        mediaPlaybackRequiresUserAction={false} // Permitir reprodução de mídia sem ação do usuário
+        onMessage={(event) => console.log(event.nativeEvent.data)}
+        onPermissionRequest={(request: any) => {
+          if (request.resources.includes('android.webkit.resource.VIDEO_CAPTURE')) {
+            request.grant(request.resources); // Conceder permissão de captura de vídeo
+          }
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +54,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    marginTop: 50
+  }
 });
